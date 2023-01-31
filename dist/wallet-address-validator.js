f814f1738b351037180ca169d26d5cc5b0bb5f4a
BruZ <16328168+oszis@users.noreply.github.com>
2023-01-31 12:55:23 +0700

    fix near

--- dist/wallet-address-validator.js
+++ dist/wallet-address-validator.js
@@ -23609,11 +23609,7 @@ const ACCOUNT_ID_REGEX =
     /^(([a-z\d]+[-_])*[a-z\d]+\.)*([a-z\d]+[-_])*[a-z\d]+$/;
 
 function isValidAddress(accountId) {
-    return (
-        accountId.length >= 2 &&
-        accountId.length <= 64 &&
-        ACCOUNT_ID_REGEX.test(accountId)
-    );
+    return ACCOUNT_ID_REGEX.test(accountId);
 }
 
 module.exports = {
